097489ba25848a05f1c5050dc97efee25fe5223a
Jie_Hu <bonehj@163.com>
2017-11-03 09:01:48 +0800

    wx

--- wechat/admin.py
+++ wechat/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import MUser
 # Register your models here.
+
+class MUserAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'user_name', 'user_description', 'user_follower_counts','user_friends_count','user_gender','user_province','user_stocks_count','user_is_deal')
+    search_fields = ('user_name',)
+
+admin.site.register(MUser,MUserAdmin)
--- wechat/models.py
+++ wechat/models.py
@@ -8,6 +8,9 @@ class Student(models.Model):
     stu_name = models.CharField(max_length=100)
     stu_class = models.CharField(max_length=20)
     stu_grade = models.CharField(max_length=20)
+    class Meta:
+        verbose_name = '学生'
+        verbose_name_plural = "学生"
 
 class MUser(models.Model):
     user_id = models.CharField(max_length=20,verbose_name='id')
@@ -19,6 +22,10 @@ class MUser(models.Model):
     user_province = models.CharField(max_length=20,verbose_name='省份',blank=True,null=True)
     user_stocks_count = models.IntegerField(verbose_name='股票数',blank=True,null=True,default=0)
     user_is_deal = models.BooleanField(verbose_name='是否处理',default=False)
+
+    class Meta:
+        verbose_name = '用户'
+        verbose_name_plural = "用户"
     
 class MArticle(models.Model):
     article_title = models.CharField(max_length=400,verbose_name='标题')
@@ -26,3 +33,7 @@ class MArticle(models.Model):
     article_author = models.CharField(max_length=100,verbose_name='作者')
     article_author_id = models.CharField(max_length=20,verbose_name='作者id')
     article_url = models.URLField(verbose_name='文章地址')
+
+    class Meta:
+        verbose_name = '文章'
+        verbose_name_plural = "文章"
